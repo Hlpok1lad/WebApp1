fbc488d50462918b61172632f8938dab78ad0c77
Hlpok1lad <yuriykhomenkov@gmail.com>
2026-03-03 16:24:33 +0100

    Add html

--- web.py
+++ web.py
@@ -9,7 +9,8 @@ def add_todo():
 
 st.title('My Todo App')
 st.subheader('This is my todo app')
-st.write('This is app for increasing your productivity')
+st.write('This is app for increasing your <b>productivity</b>',unsafe_allow_html=True)
+st.text_input(label='Add Todo',placeholder='Add a todo...',on_change=add_todo,key='new_todo')
 for index,todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
     if checkbox:
@@ -20,4 +21,3 @@ for index,todo in enumerate(todos):
 
 
 
-st.text_input(label='Add Todo',placeholder='Add a todo...',on_change=add_todo,key='new_todo')
